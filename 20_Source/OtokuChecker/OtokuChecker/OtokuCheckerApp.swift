53c6bb790e9bfc4f055ac9ba51d5e91a19db4c2c
ShuheiIshihara <shuheinojobhunting@gmail.com>
2025-08-19 01:05:21 +0900

    プロジェクトを追加しました

---
+++ 20_Source/OtokuChecker/OtokuChecker/OtokuCheckerApp.swift
@@ -0,0 +1,20 @@
+//
+//  OtokuCheckerApp.swift
+//  OtokuChecker
+//
+//  Created by 石原脩平 on 2025/08/19.
+//
+
+import SwiftUI
+
+@main
+struct OtokuCheckerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
